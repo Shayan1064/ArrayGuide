cdfd11ab1bb0abd7fc6b06e136e7c7f4060f426c
Shayan1064 <gamingfanatics2115@gmail.com>
2025-05-01 14:22:24 +0500

    Insserrtion at the end

---
+++ insertioninarrayatend.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void insertion(int array[],int size,int item){
+array[size]=item;
+size++;
+cout<<"Upgraded array"<<endl;
+for (int i = 0; i < size; i++)
+{
+   cout<< array[i];
+}
+}
+int main(){
+    int count;
+    cout<<"Enter the size of an array"<<endl;
+    cin>>count;
+
+    int array[count];
+    cout<<"Enter the elements into array"<<endl;
+    for (int i = 0; i < count; i++)
+    {
+        cin>>array[i];
+    }
+    int element;
+    cout<<"Enter the element you want to insert"<<endl;
+    cin>>element;
+insertion(array,count,element);
+
+
+
+}
