ac0ff41c9e0b114165c48dc4fa2ec00028fcbe8c
Shayan1064 <gamingfanatics2115@gmail.com>
2025-05-03 00:56:00 +0500

    Deletion at the beginning

---
+++ DeletionAtStart.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int count;
+    cout<<"Enter the Array size"<<endl;
+    cin>>count;
+    int arr[count];
+    cout<<"Enter Array elements"<<endl;
+    for (int i = 0; i < count; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Elements after deletion"<<endl;
+for (int i = 0; i < count; i++)
+{
+    arr[i-1]=arr[i];
+    
+}
+count--;
+for (int i = 0; i < count; i++)
+{
+    cout<<arr[i];
+}
+
+}
